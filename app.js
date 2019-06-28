9c86f5f84f42b7dadcbec6f8c8c081e854a7ef42
Belmin Ruhotina <44238254+beelmin@users.noreply.github.com>
2019-06-28 15:30:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.get('/', function (req, res,next) {
 	
 });
 
+
 app.post('/', function(req, res, next) {
 	if(req.body.authentication){
 		
@@ -52,11 +53,47 @@ app.post('/', function(req, res, next) {
 			    
 			    if(result.length == 0){
 			    	response = {success: false}
+			    	res.send(response);
 			    }
 			    else{
-			    	response = {success: true}
+
+
+			    	const instance2 = new MongoClient(uri, { useNewUrlParser: true });
+						instance2.connect((err, client) => {
+						  if (err){
+						  	console.log('failed to connect')
+						  	console.log(err)
+						  } 
+					  		else {
+				    
+				    		const collection2 = client.db("android_baza").collection("korisnici");
+
+		  					var query = { "username": req.body.username };
+		  					var newvalues = { $set: {online: true} };
+			    	
+							collection2.updateOne(query,newvalues, function(err, result) {
+						
+								if (err) throw(err)
+						
+
+								if(result){
+									response = {success: true}
+								}else{
+									response = {success : false}
+								}
+					
+								res.send(response);
+
+					
+							});
+					     
+				    		instance2.close()
+				        
+				  		}
+				 		});
+
 			    }
-			    res.send(response);
+			    
 			    
 			  });
 		    		 
